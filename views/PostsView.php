4f034b38df1ab954d797d27d46b738f07c15dede
Franck <francklesuedois@gmail.com>
2023-04-30 15:21:07 +0200

    escape output4

--- views/PostsView.php
+++ views/PostsView.php
@@ -5,20 +5,16 @@ require_once('./views/common/headerPosts.php');
 <div class="bg-light">
     <div class="container py-4">
         <?php $datas = array_reverse($datas);
-        foreach ($datas as $ligne) : 
-        $title = htmlspecialchars($ligne['title']);
-        $summary = htmlspecialchars($ligne['summary']);
-        $username = htmlspecialchars($ligne['username']);
-        $creationDate = htmlspecialchars($ligne['creation_date']);
-        $idPost = htmlspecialchars($ligne['id_post']);
+        foreach ($datas as $ligne):
+        $title = $ligne['title']; $summary = $ligne['summary']; $username = $ligne['username']; $creationDate = $ligne['creation_date']; $idPost = $ligne['id_post'];
         ?>
             <div class="card mb-3">
                 <div class="card-body">
-                    <h2 class="card-title"><?= $title ?></h2>
-                    <p class="card-text"><?= $summary ?></p>
+                    <h2 class="card-title"><?= htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?></h2>
+                    <p class="card-text"><?= htmlspecialchars($summary, ENT_QUOTES, 'UTF-8') ?></p>
                     <div class="d-flex align-items-center justify-content-between">
-                        <p>Par <?= $username ?> le <?= $creationDate ?></p>
-                        <a href="article&amp;id=<?= $idPost ?>" class="btn btn-secondary stretched-link rounded">Lire l'article</a>
+                        <p>Par <?= htmlspecialchars($username, ENT_QUOTES, 'UTF-8') ?> le <?= htmlspecialchars($creationDate, ENT_QUOTES, 'UTF-8') ?></p>
+                        <a href="article&amp;id=<?= htmlspecialchars($idPost, ENT_QUOTES, 'UTF-8') ?>" class="btn btn-secondary stretched-link rounded">Lire l'article</a>
                     </div>
                 </div>
             </div>
